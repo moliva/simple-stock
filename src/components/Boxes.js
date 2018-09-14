34fd3d1310dac7d2091aac1d611c91f47440ad06
miguel <miguel.oliva@olx.com>
2018-09-14 01:05:38 -0300

    Adding styles buttons

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import cssModules from 'react-css-modules'
-// import styles from '../style/boxes.scss'
+import styles from '../style/boxes.scss'
 
-// @cssModules(styles)
+@cssModules(styles)
 export default class Boxes extends Component {
   static propTypes = {
     boxes: PropTypes.array.isRequired,
@@ -14,22 +14,24 @@ export default class Boxes extends Component {
     const { boxes, styles, boxNumber } = this.props;
 
     const stylesBox = [
-      { class: 'btn-warning', style: { backgroundColor: '#ace600'} },
-      { class: 'btn-warning', style: { backgroundColor: '#993399' } },
-      { class: 'btn-warning', style: { backgroundColor: '#ffffcc' } },
-      { class: 'btn-warning', style: { backgroundColor: '#ffcccc' } },
-      { class: 'btn-warning', style: {  } },
+      { class: styles.btnBox1, style: { } },
+      { class: styles.btnBox2, style: { } },
+      { class: styles.btnBox3, style: { } },
+      { class: styles.btnBox4, style: { } },
+      { class: 'btn-warning', style: { } },
       { class: 'btn-info', style: { color: '#000000' } },
       { class: 'btn-info progress-bar-striped', style: { color: '#000000' } },
-      { class: 'btn-warning progress-bar-striped', style: {  } }
+      { class: 'btn-warning progress-bar-striped', style: { } }
     ]
+
+    console.log(styles)
 
     return (
       <ul className='nav nav-pills nav-fill'>
         {boxes.map(box =>
           <li className='nav-item' key={'menu-' + box.number}>
-            <a href={boxNumber == box.number ? '/' : box.number} className={'nav-link border ' + stylesBox[box.number - 1].class
-             + (boxNumber == box.number ? ' active' : '')
+            <a id='btnBox1' href={boxNumber == box.number ? '/' : box.number} className={
+              stylesBox[box.number - 1].class + (boxNumber == box.number ? ' ' + styles.active : '') + ' nav-link border'
             } 
              style={stylesBox[box.number - 1].style}>{box.number}</a>
           </li>
