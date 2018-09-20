11cbff286fa9131171aa6203c2d9ea5808e48d70
miguel <miguel.oliva@olx.com>
2018-09-20 01:06:24 -0300

    Adding clear search

--- src/components/Search.js
+++ src/components/Search.js
@@ -12,6 +12,7 @@ export default class Search extends Component {
     super(props)
 
     this.handleChange = this.handleChange.bind(this);
+    this.onClear = this.onClear.bind(this);
   }
 
   componentWillMount() {
@@ -27,12 +28,23 @@ export default class Search extends Component {
     this.props.dispatch(updateFilter(newValue))
   }
 
+  onClear(event) {
+    const newValue = ''
+    const newState = { value: newValue }
+
+    this.setState(newState)
+    this.props.dispatch(updateFilter(newValue))
+  }
+
   render() {
     // const { filter } = this.props
 
     return (
         <div className='input-group mb-3'>
           <input type="search" className='form-control' value={this.state.value} onChange={this.handleChange} />
+          <div className="input-group-append">
+            <a href="#" className='input-group-text btn btn-secondary' onClick={this.onClear}>X</a>
+          </div>
         </div>
     )
   }
