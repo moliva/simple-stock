2a9922a07cfb2a35507258f476d2d52b097dd62f
moliva <oliva.miguelh@gmail.com>
2019-12-28 12:57:45 -0300

    removing everything

--- src/store/configureStore.js
+++
@@ -1,8 +0,0 @@
-const { NODE_ENV } = process.env;
-
-if (NODE_ENV === 'production' || NODE_ENV === 'test') {
-  module.exports = require('./configureStore.prod');
-}
-else {
-  module.exports = require('./configureStore.dev');
-}
