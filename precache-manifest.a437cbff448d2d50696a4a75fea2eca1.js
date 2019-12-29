c72b7ef8049a68d5f21480f83216884390ad08f9
moliva <oliva.miguelh@gmail.com>
2019-12-29 12:03:07 -0300

    a

---
+++ precache-manifest.a437cbff448d2d50696a4a75fea2eca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6c5f6b5a079d15709290e5c4b1c257",
+    "url": "/simple-stock/index.html"
+  },
+  {
+    "revision": "d40b8ee6a5cf97092581",
+    "url": "/simple-stock/static/css/main.b3a5f09b.chunk.css"
+  },
+  {
+    "revision": "47815cfaa6aef6e89901",
+    "url": "/simple-stock/static/js/2.269a5c79.chunk.js"
+  },
+  {
+    "revision": "81967b8a233bd3d8bb163037a4843f82",
+    "url": "/simple-stock/static/js/2.269a5c79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d40b8ee6a5cf97092581",
+    "url": "/simple-stock/static/js/main.ddd77add.chunk.js"
+  },
+  {
+    "revision": "dac69fbacc8a420efce8",
+    "url": "/simple-stock/static/js/runtime-main.24dbeb4f.js"
+  }
+]);
